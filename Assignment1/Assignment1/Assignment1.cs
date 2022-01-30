0784f9cc4be5cb48e7261e0d987a4be44ee60986
comandcenter <tsfhcomandcenter@gmail.com>
2022-01-30 13:41:13 +0900

    COMP1500 Ass1 edit1

--- Assignment1/Assignment1/Assignment1.cs
+++ Assignment1/Assignment1/Assignment1.cs
@@ -7,24 +7,19 @@ namespace Assignment1
     {
         public static void PrintIntegers(StreamReader input, StreamWriter output, int width)
         {
-            if(width <= 9)
+            if (width <= 9)
             {
                 width = 10;
             }
 
-            output.Write("Type 1st integer : ");
             string num1 = input.ReadLine().Trim();
             int int1 = int.Parse(num1);
-            output.Write("Type 2nd integer : ");
             string num2 = input.ReadLine().Trim();
             int int2 = int.Parse(num2);
-            output.Write("Type 3rd integer : ");
             string num3 = input.ReadLine().Trim();
             int int3 = int.Parse(num3);
-            output.Write("Type 4th integer : ");
             string num4 = input.ReadLine().Trim();
             int int4 = int.Parse(num4);
-            output.Write("Type 5th integer : ");
             string num5 = input.ReadLine().Trim();
             int int5 = int.Parse(num5);
 
@@ -38,29 +33,24 @@ namespace Assignment1
 
         public static void PrintStats(StreamReader input, StreamWriter output)
         {
-            output.Write("Type 1st number : ");
             string num1 = input.ReadLine().Trim();
             double double1 = double.Parse(num1);
 
-            output.Write("Type 2nd number : ");
             string num2 = input.ReadLine().Trim();
             double double2 = double.Parse(num2);
             double max = Math.Max(double1, double2);
             double min = Math.Min(double1, double2);
 
-            output.Write("Type 3rd number : ");
             string num3 = input.ReadLine().Trim();
             double double3 = double.Parse(num3);
             max = Math.Max(double3, max);
             min = Math.Min(double3, min);
 
-            output.Write("Type 4th number : ");
             string num4 = input.ReadLine().Trim();
             double double4 = double.Parse(num4);
             max = Math.Max(double4, max);
             min = Math.Min(double4, min);
 
-            output.Write("Type 5th number : ");
             string num5 = input.ReadLine().Trim();
             double double5 = double.Parse(num5);
             max = Math.Max(double5, max);
