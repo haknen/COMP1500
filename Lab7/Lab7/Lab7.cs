6428926c713e9ebf8b53a41c85e3598fc31d52de
comandcenter <tsfhcomandcenter@gmail.com>
2022-03-07 05:34:44 +0900

    COMP1500 Lab7

---
+++ Lab7/Lab7/Lab7.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Lab7
+{
+    internal class Lab7
+    {
+        public static bool PlayGame(uint[] array)
+        {
+            if (array.Length <= 1 || array[0] >= array.Length)
+            {
+                return false;
+            }
+
+            uint[] callCount = new uint[array.Length];
+
+            return Search(array, callCount, array[0]);
+        }
+
+        public static bool Search(uint[] array, uint[] callCount, uint position)
+        {
+            callCount[position]++;
+            uint left = position > array[position] ? position - array[position] : 0;
+            uint right = position + array[position];
+            if (position == array.Length - 1)
+            {
+                return true;
+            }
+
+            if (position < 1 || position >= array.Length ||
+                callCount[position] >= array.Length)
+            {
+                return false;
+            }
+
+            if (left > 0 && right < array.Length)
+            {
+                return Search(array, callCount, left) ||
+                Search(array, callCount, right);
+            }
+
+            if (left < 1 && right < array.Length)
+            {
+                return Search(array, callCount, right);
+            }
+
+            if (left > 0 && right >= array.Length)
+            {
+                return Search(array, callCount, left);
+            }
+
+            return false;
+        }
+    }
+}
